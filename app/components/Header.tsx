790382b68af42b3c8c6c834054343e686ea44016
Lucas Lourenço <lucascelestiano@gmail.com>
2024-04-15 13:17:03 -0400

    add mobile icon

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-center px-4 sticky inset-0">
+    <header className="md:flex items-center justify-center px-6 sticky inset-0">
       <div className="max-w-[1180px] w-full flex items-center justify-between h-24">
         <Image src="/logo.png" width={64} height={64} alt="" />
 
-        <nav className="flex items-center justify-center list-none gap-8 font-bold uppercase text-lg text-dark-blue-400">
+        <button className="md:hidden text-dark-blue-400">
+          <FaBars size={32} />
+        </button>
+
+        <nav className="md:flex hidden items-center justify-center list-none gap-8 font-bold uppercase text-lg text-dark-blue-400">
           <li>
             <Link href="" className="hover:text-orange-400 transition-colors">
               Home
